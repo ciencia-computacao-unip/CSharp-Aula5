6a0e7cf9909b94aa84d1d0bc2fab860053d508c8
Victor Perin <victor_perin@nerdhitech.com.br>
2014-08-30 13:49:15 -0300

    Arquivos básicos

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace aula5{
+    class Program{
+        static void Main(string[] args){
+
+        }
+    }
+}
