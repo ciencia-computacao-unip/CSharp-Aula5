d754b76455c0a5ccf1e7b3f159249916c4c24f1b
Victor Perin <victor_perin@nerdhitech.com.br>
2014-08-31 13:51:12 -0300

    exercício 1

--- Program.cs
+++ Program.cs
@@ -6,6 +6,14 @@ using System.Text;
 namespace aula5{
     class Program{
         static void Main(string[] args){
+            //inicio exercício 1
+            Console.WriteLine("1) Escreva um programa que exiba na tela em ordem crescente, apenas os números pares existentes de 11 a 250.\n");
+            for (int x = 11; x <= 250; x++){
+                if (x % 2 == 0){
+                    Console.WriteLine(x);
+                }
+            }
+            //fim exercício 1
 
             Console.ReadKey();
         }
