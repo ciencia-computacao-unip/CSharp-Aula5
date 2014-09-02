ac55842f2282232f2ecb384edb45f38fc46f8020
Victor Perin <victor_perin@nerdhitech.com.br>
2014-09-02 17:23:21 -0300

    ex 5

--- Program.cs
+++ Program.cs
@@ -48,7 +48,7 @@ namespace aula5{
 
             //início exercício 4
             Console.WriteLine("4) Escreva um programa que exiba na tela a quantidade de números ímpares existentes entre dois números que o usuário digitar (testar inclusive os números digitados).\n");
-            Console.Write("Digite úm número: ");
+            Console.Write("Digite um número: ");
             int primeiro_numero,segundo_numero,quantidade_numeros_impares=0,menor_numero,maior_numero;
             primeiro_numero = int.Parse(Console.ReadLine());
             Console.Write("Digite outro número: ");
@@ -65,6 +65,22 @@ namespace aula5{
             }
             Console.WriteLine("Quantidade de números ímpares entre {0} e {1} é {2}",menor_numero,maior_numero,quantidade_numeros_impares);
             //fim exercício 4
+
+            Console.WriteLine("\n\n\n");
+
+            //início exercício 5
+            Console.WriteLine("5) Escreva um programa que leia 15 números inteiros e exiba na tela ao final, o maior número que foi digitado pelo usuário.\n");
+            int numeros_ex5, maior_numero_ex5=0;
+            for (int x = 1; x <= 15; x++){
+                Console.Write("Digite o {0}° número: ",x);
+                numeros_ex5 = int.Parse(Console.ReadLine());
+                if (numeros_ex5 > maior_numero_ex5){
+                    maior_numero_ex5 = numeros_ex5;
+                }
+            }
+            Console.WriteLine("\nO maior número digitado foi: {0}", maior_numero_ex5);
+            //fim exercício 5
+
 
             Console.ReadKey();
         }
